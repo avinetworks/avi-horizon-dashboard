1891b07b0a4a4cb9df85acdd6e3b3f4d87359018
Praveen Yalagandula <ypraveen@avinetworks.com>
2016-03-08 22:06:51 +0000

    Script to compile and upload pip packages to github

---
+++ create_release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+assets=""
+git tag -d latest
+git tag latest
+git push -f origin latest
+set -e
+for BRANCH in juno kilo master
+do
+    git checkout -B $BRANCH
+    rm -rf dist/
+    python setup.py sdist
+    mv dist/*tar.gz avidashboard-$BRANCH.tar.gz
+    assets="$assets -a avidashboard-$BRANCH.tar.gz#pip-package-$BRANCH"
+done
+/root/bin/hub release edit $assets -F ReleaseNote latest
