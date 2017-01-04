5a647c99e3a9659360b47374ebb1ebcc9b64f7c2
Praveen Yalagandula <ypraveen@avinetworks.com>
2017-01-04 00:55:16 +0000

    Moving to using ApiSession from Avi SDK
    Also support for keystone v3 (username@userdomain)

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+setup(
     setup_requires=['pbr'],
     pbr=True)
