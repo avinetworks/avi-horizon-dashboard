57e33daf48cd000f4c152797404e04c8b63eb4a7
Praveen Yalagandula <ypraveen@avinetworks.com>
2017-07-24 22:22:52 +0000

    Updated release script

--- create_release.sh
+++ create_release.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 set -x
 assets=""
-git tag -d latest
-git tag latest
-git push -f origin latest
+#git tag -d latest
+#git tag latest
+#git push -f origin latest
 set -e
-for BRANCH in juno kilo master newton
+for BRANCH in tabs panel
 do
+    rm -rf VERSION
     git checkout $BRANCH
     git pull --rebase origin $BRANCH
-    rm -rf dist/
+    rm -rf dist/ debian/ build/ ../*avidashboard*deb
     python setup.py sdist
-    mv dist/*tar.gz avidashboard-$BRANCH.tar.gz
-    assets="$assets -a avidashboard-$BRANCH.tar.gz#pip-package-$BRANCH"
+    fname=`ls dist`
+    mv dist/$fname ./
+    assets="$assets -a $fname#pip-package-$BRANCH"
+
+    # debian package
+    python setup.py --command-packages=stdeb.command debianize
+    dpkg-buildpackage -b -us -uc -d
+    fname=`ls ../*avidashboard*deb`
+    assets="$assets -a $fname#debian-package-$BRANCH"
+    rm -rf debian/ build/ ../*avidashboard*changes
+    cp $fname ./
 done
-/root/bin/hub release edit $assets -F ReleaseNote latest
+git checkout master
+echo "$assets"
+#/root/utils/hub release delete latest
+#/root/utils/hub release create $assets -F ReleaseNote latest
